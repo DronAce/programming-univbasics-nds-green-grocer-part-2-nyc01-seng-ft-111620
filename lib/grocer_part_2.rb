1d987502c610eb53457f62e3c09a220066c77cd0
Okechukwu Uneze <unezeo@gmail.com>
2020-11-11 14:47:18 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,40 @@
 require_relative './part_1_solution.rb'
+require "pry"
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart_with_coupons = []
+  hash_cart = {}
+  cart.each do |items|
+    coupons.each do |coupon|
+      name = coupon[:item]
+      quantity = items[:count]]
+      counter = 0
+      cost = coupon[:cost] / coupon[:num]
+      if items[:item] == name && quantity > coupon[:num]
+        while items[:item] == name && items[:count] >= coupon[:num] do
+          items[:count] -= coupon[:num]
+          counter += 1
+          hash_cart = items
+          hash_cart[:item] = "#{name} W/COUPON"
+          hash_cart[:count] = coupon[:num] * counter
+          hash_cart[:price] = cost
+          cart_with_coupons << hash_cart
+        end
+        while quantity > coupon[:num] do
+          quantity = quantity - coupon[:num]
+        end
+        hash_cart = items
+        hash_cart[:count] = quantity
+        cart_with_coupons << hash_cart
+        binding.pry
+      end
+
+    end
+  end
+  cart_with_coupons
 end
 
 def apply_clearance(cart)
