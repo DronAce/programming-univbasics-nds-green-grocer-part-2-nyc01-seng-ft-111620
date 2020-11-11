ebc18bc5255b78e3bde2340c5878167e90eb5210
Okechukwu Uneze <unezeo@gmail.com>
2020-11-11 15:33:22 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,7 +13,7 @@ def apply_coupons(cart, coupons)
       quantity = items[:count]
       counter = 0
       cost = (coupon[:cost] / coupon[:num]).round(2)
-      if items[:item] == name && items[:count] / coupon[:num] >= 1 
+      if items[:item] == name && items[:count] / coupon[:num] >= 1
         hash_cart = items
         hash_cart[:item] = "#{name} W/COUPON"
         hash_cart[:count] = items[:count] - (items[:count] % coupon[:num])
