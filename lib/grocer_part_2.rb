5ad2496690a49d9f276fd6d5b2cf3bf12667adb7
Okechukwu Uneze <unezeo@gmail.com>
2020-11-11 15:52:39 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -44,5 +44,5 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-
+  apply_coupons(cart, coupons)
 end
