71fdf0a2ca04cd91d2e393d06c2a588251aa7386
Okechukwu Uneze <unezeo@gmail.com>
2020-11-11 15:45:22 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,41 +5,22 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  cart_with_coupons = []
-  hash_cart = {}
-  cart.each do |items|
-    coupons.each do |coupon|
-      name = coupon[:item]
-      quantity = items[:count]
-      cost = (coupon[:cost] / coupon[:num]).round(2)
-      if items[:item] == name && items[:count] / coupon[:num] >= 1
-        items[:item] = "#{name} W/COUPON"
-        hash_cart[:count] = items[:count] - (items[:count] % coupon[:num])
-        hash_cart[:price] = cost
-        cart_with_coupons << hash_cart
-      elsif items[:item] == name && items[:count] % coupon[:num] >= 1
-        items[:count] = items[:count] % coupon[:num]
+  coupons_index = 0
+  
+  while coupons_index < coupons.size do
+    current_coupon = coupons[coupons_index]
+    applicable_for_discount = find_item_by_name_in_collection( current_coupon[:item], cart )
+      if ( applicable_for_discount[:count] / current_coupon[:num] >= 1 )
+        cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
+                    :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
+                    :clearance => applicable_for_discount[:clearance],
+                    :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % current_coupon[:num])})
+  
+        applicable_for_discount[:count] %= current_coupon[:num]
       end
-    end
+    coupons_index += 1
   end
   cart
-  # coupons_index = 0
-  #
-  # while coupons_index < coupons.size do
-  #   current_coupon = coupons[coupons_index]
-  #   applicable_for_discount = find_item_by_name_in_collection( current_coupon[:item], cart )
-  #     if ( applicable_for_discount[:count] / current_coupon[:num] >= 1 )
-  #       cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
-  #                   :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
-  #                   :clearance => applicable_for_discount[:clearance],
-  #                   :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % current_coupon[:num])})
-  #
-  #       applicable_for_discount[:count] %= current_coupon[:num]
-  #     end
-  #   coupons_index += 1
-  # end
-  # cart
-
 end
 
 def apply_clearance(cart)
